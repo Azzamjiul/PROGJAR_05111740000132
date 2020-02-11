df054f63cddb389cf8053ea7e818df32aaa10ace
azzam <azzam.jiul@gmail.com>
2020-02-11 11:06:37 +0700

    change to python3

--- progjar1/client.py
+++ progjar1/client.py
@@ -6,23 +6,28 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
 server_address = ('localhost', 8000)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
+# print >>sys.stderr, 'connecting to %s port %s' % server_address
+print(sys.stderr, 'starting up on %s port %s' % server_address)
 sock.connect(server_address)
 
 try:
     # Send data
     message = 'INI ADALAH DATA YANG DIKIRIM ABCDEFGHIJKLMNOPQ'
-    print >>sys.stderr, 'sending "%s"' % message
-    sock.sendall(message)
+    # print >>sys.stderr, 'sending "%s"' % message
+    print(sys.stderr, 'sending ', message)
+    sock.sendall(message.encode())
     # Look for the response
     amount_received = 0
     amount_expected = len(message)
-    print "%d" % amount_expected
+    # print "%d" % amount_expected
+    # print(amount_expected)
     while amount_received < amount_expected:
         data = sock.recv(16)
         amount_received += len(data)
-        print "amount_received = %d" % amount_received
-        print >>sys.stderr, 'received "%s"' % data
+        # print "amount_received = %d" % amount_received
+        # print >>sys.stderr, 'received "%s"' % data
+        print(sys.stderr, 'received "%s"' % data)
 finally:
-    print >>sys.stderr, 'closing socket'
+    # print >>sys.stderr, 'closing socket'
+    print(sys.stderr, 'closing socket')
     sock.close()
